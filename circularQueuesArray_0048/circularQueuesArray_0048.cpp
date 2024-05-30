9e8114c398c0bb5fe0678e8ee78a07ad09ac7c1f
nachool <nachma.olivia1394@gmail.com>
2024-05-30 18:15:06 +0700

    case 2

--- circularQueuesArray_0048/circularQueuesArray_0048.cpp
+++ circularQueuesArray_0048/circularQueuesArray_0048.cpp
@@ -123,6 +123,10 @@ int main()
                 q.insert();
                 break;
             }
+            case '2': {
+                q.remove();
+                break;
+            }
 
 }
 
