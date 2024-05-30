40b8219223160af4687eb37309ddb8c1bd26b9a9
nachool <nachma.olivia1394@gmail.com>
2024-05-30 17:49:34 +0700

    ulangi front hingga akhior array

--- circularQueuesArray_0048/circularQueuesArray_0048.cpp
+++ circularQueuesArray_0048/circularQueuesArray_0048.cpp
@@ -84,6 +84,12 @@ public:
             cout << endl;
         }
         else {
+            //Jika FRONT > REAR, iterasi dari FRONT hingga akhir array
+            while (FRONT_Position <= max - 1) {
+                cout << queues_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+
 
 
 
