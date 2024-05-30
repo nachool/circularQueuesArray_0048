c058a18cf51aa57cdf9ee146c37375c36140a62a
nachool <nachma.olivia1394@gmail.com>
2024-05-30 17:33:59 +0700

    menambah konstruktor untuk inisiasi queue

--- circularQueuesArray_0048/circularQueuesArray_0048.cpp
+++ circularQueuesArray_0048/circularQueuesArray_0048.cpp
@@ -5,6 +5,13 @@ class Queues {
     int FRONT, REAR, max = 5;
     int queues_array[5];
 
+public:
+    // Constructor untuk inisialisasi queue
+    Queues() {
+        FRONT = -1;
+        REAR = -1;
+    }
+
 
 
 int main()
