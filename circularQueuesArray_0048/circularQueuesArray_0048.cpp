05e187c5a4c70db679b8fb824b661854d0157484
nachool <nachma.olivia1394@gmail.com>
2024-05-30 18:14:43 +0700

    menambah switch case 1

--- circularQueuesArray_0048/circularQueuesArray_0048.cpp
+++ circularQueuesArray_0048/circularQueuesArray_0048.cpp
@@ -117,6 +117,12 @@ int main()
             cin >> ch;
             cout << endl;
 
+            switch (ch) {
+            case '1': {
+
+                q.insert();
+                break;
+            }
 
 }
 
